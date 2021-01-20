dff9d49df6d5ce4dec6c9435e33c64074a9b9556
Rishabh Raghwendra <rishabhraghwendra2002@gmail.com>
2021-01-20 10:12:02 +0530

    Card aligned center and setted perfect width

---
+++ droidspeak/src/components/LoginSignUp.js
@@ -0,0 +1,14 @@
+import {Card} from 'react-bootstrap';
+import '../styles/LoginSignUp.css';
+function LoginSignUp() {
+return (
+      <Card>
+            <Card.Body>
+                  <Card.Title><i class="fa fa-user-o" aria-hidden="true"></i></Card.Title>
+                  <Card.Subtitle>Login</Card.Subtitle>
+                  
+            </Card.Body>
+      </Card>
+);
+}
+export default LoginSignUp;
--- droidspeak/src/index.js
+++ droidspeak/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+import './styles/index.css'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
