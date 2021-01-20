5909fc25b0b89e249cad05518994d50541bc0595
Rishabh Raghwendra <rishabhraghwendra2002@gmail.com>
2021-01-20 20:26:34 +0530

    Login button style not working

--- droidspeak/src/components/LoginSignUp.js
+++ droidspeak/src/components/LoginSignUp.js
@@ -6,12 +6,18 @@ function LoginSignUp() {
             <Card>
                   <Card.Body>
                         <Card.Title><i class="fa fa-user-o" aria-hidden="true"></i></Card.Title>
-                        <Card.Subtitle>Login</Card.Subtitle>
+                        <h6 className="card-subtitile">Login</h6>
                         <Card.Text>
-                        <label>
-                              <i class="fa fa-user-o" aria-hidden="true"></i>
-                              <input type="email" placeholder="Enter your email id"/>
+                              <label>
+                                    <i class="fa fa-user" aria-hidden="true"></i>
+                                    <input type="email" className="my-2" placeholder="Enter your email id"/>
                               </label>
+                              <label>
+                                    <i class="fa fa-lock" aria-hidden="true"></i>
+                                    <input type="password" className="my-2" placeholder="Enter your password"/>
+                              </label>
+                              <button type="button" >LOGIN</button>
+                              <div className="circle"></div>
                         </Card.Text>
                   </Card.Body>
             </Card>
