ea9795fed55b1ebf3ca4ebfa5df7970db5dbfe74
Rishabh Raghwendra <rishabhraghwendra2002@gmail.com>
2021-01-23 19:31:49 +0530

    platform choose dropdown successfully applied with styles

--- droidspeak/src/components/ScheduleModal.js
+++ droidspeak/src/components/ScheduleModal.js
@@ -1,12 +1,17 @@
 import {Modal,Button} from 'react-bootstrap';
-import React,{useState} from 'react'
+import React,{useState} from 'react';
+import {db}from '../utilites/firebase';
 
 export default function ScheduleModal({show,handleClose}) {
-      // const[time,setTime]=useState();
-      let time=null;
-      const Time=function(event){
-            event.preventDefault();
-            // console.log(time);
+      const[stime,setSTime]=useState();
+      const [etime,setETime]=useState();
+      const[meetingname,setMeetingname]=useState();
+      const[date,setDate]=useState();
+      const [platform,setPlatform]=useState("zoom");
+      const style={
+            "border-radius": "5px",
+            "padding": "3px",
+            "background-color": "white"
       }
       return (
             <Modal
@@ -22,19 +27,28 @@ export default function ScheduleModal({show,handleClose}) {
                       <table>
                             <tr>
                                   <td>Meeting Name:</td>
-                                  <td><input type="text" className="my-2"/></td>
+                                  <td><input type="text" className="my-2" onChange={e=>setMeetingname(e.target.value)}/></td>
                             </tr>
                             <tr>
                                   <td>Meeting Date:</td>
-                                  <td><input type="date" className="my-2" onChange={e=>console.log(e.target.value)}/></td>
+                                  <td><input type="date" className="my-2" onChange={e=>setDate(e.target.value)}/></td>
                             </tr>
                             <tr>
                                   <td>Start Time:</td>
-                                  <td><input type="time" onChange={e=>console.log(e.target.value)}/></td>
+                                  <td><input type="time" onChange={e=>setSTime(e.target.value)}/></td>
                             </tr>
                             <tr>
                                   <td>End Time:</td>
-                                  <td><input type="time" className="my-2"/></td>
+                                  <td><input type="time" className="my-2" onChange={e=>setETime(e.target.value)}/></td>
+                            </tr>
+                            <tr>
+                                  <td>Choose Platform:</td>
+                                  <td>
+                                        <select name="platform" style={style} onChange={e=>setPlatform(e.target.value)}>
+                                              <option value="Zoom">zoom</option>
+                                              <option value="Google Meet">Google Meet</option>
+                                        </select>
+                                  </td>
                             </tr>
                       </table>
             </Modal.Body>
@@ -42,7 +56,7 @@ export default function ScheduleModal({show,handleClose}) {
               <Button variant="secondary" onClick={handleClose}>
                 Close
               </Button>
-              <Button variant="success" onClick={Time}>Schedule</Button>
+              <Button variant="success">Schedule</Button>
             </Modal.Footer>
           </Modal>
       )
