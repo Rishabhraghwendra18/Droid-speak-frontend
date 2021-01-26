f93a9dd6275334d5f76abd854bbce0baf9d3e697
Rishabh Raghwendra <rishabhraghwendra2002@gmail.com>
2021-01-26 08:54:56 +0530

    loader setted successfully

--- droidspeak/src/App.js
+++ droidspeak/src/App.js
@@ -3,22 +3,28 @@ import './styles/App.css';
 import {useState,useEffect}from 'react';
 import {auth}from './utilites/firebase';
 import Dashboard from './components/Dashboard';
+import ClockLoader from "react-spinners/ClockLoader";
 function App() {
+  const[loader,setLoader]=useState(false);
   const[user,setUser]=useState(false);
   useEffect(() => {
   auth.onAuthStateChanged((authUser)=>{
     if(authUser){
       setUser(true);
+      setLoader(false);
     }
     else{
       setUser(false);
-      console.log("logged out")
     }
   })
+  setLoader(true);
+
   }, [])
   return (
     <div className="App">
-      {user?<Dashboard/>:<LoginSignUp/>}
+      {loader?(<ClockLoader color={'#000000'} loading={loader} size={150} />):
+      user?<Dashboard/>:<LoginSignUp/>
+      }
     </div>
   );
 }
